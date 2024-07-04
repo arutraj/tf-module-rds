b509e87cb2cdde58289788720c4d431aad539c40
SVR Kalluru <raghuk.vit@gmail.com>
2024-07-04 08:56:03 +0530

    Expense Project Terraform

--- main.tf
+++ main.tf
@@ -1,17 +1,25 @@
-# resource "aws_db_instance" "default" {
-#   allocated_storage    = 10
-#   db_name              = "dummy-default"
-#   engine               = var.engine
-#   engine_version       = var.engine_version
-#   instance_class       = var.instance_class
-#   username             = data.vault_generic_secret.rds.data_json
-#   password             = "foobarbaz"
-#   parameter_group_name = "default.mysql8.0"
-#   skip_final_snapshot  = true
-# }
-
-resource "local_file" "test" {
-  filename = "/tmp/data.json"
-  content = data.vault_generic_secret.rds.data_json
+resource "aws_db_instance" "main" {
+  identifier           = "${var.env}-rds"
+  allocated_storage    = 10
+  db_name              = "dummy-default"
+  engine               = var.engine
+  engine_version       = var.engine_version
+  instance_class       = var.instance_class
+  username             = jsondecode(data.vault_generic_secret.rds.data_json).username
+  password             = jsondecode(data.vault_generic_secret.rds.data_json).password
+  parameter_group_name = aws_db_parameter_group.main.name
+  db_subnet_group_name = aws_db_subnet_group.main.name
+  skip_final_snapshot  = true
 }
+
+resource "aws_db_parameter_group" "main" {
+  name   = "${var.env}-rds-pg"
+  family = var.family
+}
+
+resource "aws_db_subnet_group" "main" {
+  name       = "${var.env}-rds-subnet"
+  subnet_ids = var.subnet_ids
+}
+
 
--- variables.tf
+++ variables.tf
@@ -1,1 +1,7 @@
+variable "engine" {}
+variable "engine_version" {}
+variable "instance_class" {}
+variable "env" {}
+variable "family" {}
+variable "subnet_ids" {}
 
